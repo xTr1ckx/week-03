469b1caaf5db08c956111cc42089bae4227e203f
Edgars Slipais <edgars.slipais0@gmail.com>
2026-02-24 14:54:10 +0200

    feat: add data_collections.py unfinished, so that the main branch isn't empty
    - Added data_collections.py for initial commit

---
+++ data_collections.py
@@ -0,0 +1,20 @@
+
+number_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+number_list.append(16)
+number_list.insert(2, 2.5)
+#number_list.remove(3)
+
+
+print(len(number_list))
+number_length = len(number_list)
+print("Saraksta garums ir:", number_length)
+
+print(number_list[2])
+print(number_list[-4])
+
+print(number_list)
+
+dict = {"Anna": 85, "Jānis": 72, "Līga": 95}
+
+
+list = [{"name": "Anna", "grade": 85}, {"name": "Jānis", "grade": 72}, {"name": "Līga", "grade": 95}]
